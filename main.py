36f0ffed5d4586945ae5404189c37148a429597e
Balazs Marton <balazs.marton@prezi.com>
2021-06-11 00:00:15 +0200

    init2: add modifications by course material

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
@@ -29,14 +30,23 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
+    new_data = {
+        "website": website,
+        "email": email,
+        "password": password,
+    }
+
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
                                                       f"\nPassword: {password} \nIs it ok to save?")
         if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
+            with open("data.txt", "r") as data_file:
+                data = json.load(data_file)
+                data.update(new_data)
+            with open("data.txt", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
 
