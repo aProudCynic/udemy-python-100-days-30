f2799a3d8d17bf32f49c7eff96eb5e6be354809e
Balazs Marton <balazs.marton@prezi.com>
2021-06-14 22:20:49 +0200

    feature: loading implemented

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-#Password Generator Project
 DATA_FILE_NAME = "data.txt"
 
 
@@ -24,7 +22,7 @@ def generate_password():
     password = "".join(password_list)
     password_entry.insert(0, password)
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+
 def save():
 
     website = website_entry.get()
@@ -56,21 +54,31 @@ def save():
             password_entry.delete(0, END)
 
 
+def load_data():
+    loaded_data = search()
+    if loaded_data:
+        replace_entry_value_with(email_entry, loaded_data['email'])
+        replace_entry_value_with(password_entry, loaded_data['password'])
+
+
+def replace_entry_value_with(entry, new_value):
+    entry.delete(0, len(entry.get()))
+    entry.insert(0, new_value)
+
+
 def search():
     try:
         with open(DATA_FILE_NAME, "r") as data_file:
             data = json.load(data_file)
             website = website_entry.get()
-            print(data)
             try:
                 result = data[website]
-                print(result)
+                return result
             except KeyError:
                 return None
     except FileNotFoundError:
         return None
 
-# ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
@@ -81,7 +89,6 @@ logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
-#Labels
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 email_label = Label(text="Email/Username:")
@@ -89,7 +96,6 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-#Entries
 website_entry = Entry(width=21)
 website_entry.grid(row=1, column=1)
 website_entry.focus()
@@ -99,8 +105,7 @@ email_entry.insert(0, "angela@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
-# Buttons
-search_button = Button(text="Search", command=search, width=11)
+search_button = Button(text="Search", command=load_data, width=11)
 search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=generate_password, width=11)
 generate_password_button.grid(row=3, column=2)
